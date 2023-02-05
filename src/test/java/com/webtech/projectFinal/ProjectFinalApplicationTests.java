9110296fbd7f789abe2b7f28225c431274c61ccc
s0568841 <s0568841@htw-berlin.de>
2023-02-05 18:17:11 +0100

    added packages

---
+++ src/test/java/com/webtech/projectFinal/ProjectFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtech.projectFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
